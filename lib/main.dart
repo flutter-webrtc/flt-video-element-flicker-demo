f9b65922f678a8c1a7aa9b4a480dea9ffc659e47
cloudwebrtc <duanweiwei1982@gmail.com>
2023-10-27 16:53:04 +0800

    Change to HtmlElementView to render video.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:dart_webrtc/src/media_stream_impl.dart';
+import 'dart:ui_web';
+import 'dart:html' as html;
 
 void main() {
   runApp(const MyApp());
+}
+
+class VideoElementWeb extends StatefulWidget {
+  final html.MediaStream src;
+  final int id;
+  final html.VideoElement videoElement = html.VideoElement();
+
+  VideoElementWeb(this.src, this.id, {super.key});
+
+  @override
+  VideoElementWebState createState() => VideoElementWebState();
+}
+
+class VideoElementWebState extends State<VideoElementWeb> {
+  @override
+  void initState() {
+    platformViewRegistry.registerViewFactory('video-el-web-${widget.id}',
+        (int viewId) {
+      final videoElement = widget.videoElement
+        ..style.width = '100%'
+        ..style.height = '100%'
+        ..autoplay = true
+        ..controls = false
+        ..srcObject = widget.src;
+      return videoElement;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HtmlElementView(viewType: 'video-el-web-${widget.id}');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -30,26 +66,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MediaStream? _mediaStream;
-  final List<RTCVideoRenderer> _renderers = [];
+  html.MediaStream? _mediaStream;
+  int _counter = 0;
 
   void _addVideoStream() async {
-    _mediaStream ??= await navigator.mediaDevices.getUserMedia({'video': true});
-
-    final renderer = RTCVideoRenderer();
-    await renderer.initialize();
-    renderer.srcObject = _mediaStream;
-    _renderers.add(renderer);
-
-    setState(() {});
+    if (_mediaStream == null) {
+      var fltStream =
+          await navigator.mediaDevices.getUserMedia({'video': true});
+      _mediaStream = (fltStream as MediaStreamWeb).jsStream;
+    }
+    setState(() {
+      _counter++;
+    });
   }
 
   void _removeVideoStream() async {
-    if (_renderers.isEmpty) return;
-    var renderer = _renderers.removeLast();
-    renderer.srcObject = null;
-    renderer.dispose();
-    setState(() {});
+    if (_counter == 0) return;
+    setState(() {
+      _counter--;
+    });
   }
 
   @override
@@ -64,14 +99,14 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Number of video streams: ${_renderers.length}',
+              'Number of video streams: $_counter',
             ),
             Expanded(
               child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 4,
                   ),
-                  itemCount: _renderers.length,
+                  itemCount: _counter,
                   itemBuilder: (BuildContext context, int index) {
                     return Center(
                       child: Column(
@@ -81,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             width: 320,
                             height: 180,
                             child: Center(
-                              child: RTCVideoView(_renderers[index]),
+                              child: VideoElementWeb(_mediaStream!, index),
                             ),
                           ),
                         ],
@@ -110,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: const Icon(Icons.remove),
           )
         ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
